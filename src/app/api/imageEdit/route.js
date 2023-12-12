15f79f7f72e462c82f8e0653fdde687a3ef71b36
samuelahmed <samueladuval@gmail.com>
2023-12-11 18:23:33 -0800

    push live with edge, fetch, and formdata hack

--- src/app/api/imageEdit/route.js
+++ src/app/api/imageEdit/route.js
@@ -1,9 +1,10 @@
 import OpenAI from "openai";
-import fs from "fs";
-import path from "path";
+
+import fetch from "node-fetch";
+import FormData from "form-data";
 
 //increase timeout to 25s instead of 10s
-// export const runtime = 'edge';
+export const runtime = 'edge';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,20 +12,26 @@ const openai = new OpenAI({
 
 export async function POST(request) {
   const { input } = await request.json();
-  console.log("Current directory: " + process.cwd());
+  const imageUrl =
+    "https://raw.githubusercontent.com/samuelahmed/openAItest/dfc66463df07f954fd7fd012f73f100c675f5a63/public/chiMask.png";
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
 
-  const dir = path.resolve('public/chiMask.png')
+    console.log(`Image size: ${blob.size} bytes`);
 
-  const result = await openai.images.edit({
-    
-    image: fs.createReadStream(dir),
-    prompt: input,
-  });
+  
+    const formData = new FormData();
+    formData.append("image", blob, "image.png");
+  
+    const result = await openai.images.edit({
+      image: formData.get("image"),
+      prompt: input,
+    });
+ 
+
 
   console.log("ImageEdit Called");
   console.log(result);
 
   return new Response(JSON.stringify({ image: result }), { status: 200 });
 }
-
-
