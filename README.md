21d315df32fcf29ea3cd2560d2330cdb5a769180
samuelahmed <samueladuval@gmail.com>
2023-12-11 18:45:42 -0800

    add readme and note

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-Super simple APIs calling openAI models in a barebones nextjs14 app. 
+Couple hour hack to see how hard my friend Chi's app idea is to develop. 
 
-It is deployed and you can use within reason, but please don't wreck my free credits :) 
-
-It is super rate limited anyways... and vercel edge function will timeout after 25s so long promps will fail.
-
-If you want to really do stuff you'll have to run it locally with your own API key. 
+The edge function times out after 20 seconds so the deployed version can fail periodically.
--- src/app/api/imageEdit/route.js
+++ src/app/api/imageEdit/route.js
@@ -1,37 +1,30 @@
 import OpenAI from "openai";
-
 import fetch from "node-fetch";
 import FormData from "form-data";
 
 //increase timeout to 25s instead of 10s
-export const runtime = 'edge';
+export const runtime = "edge";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(request) {
+  
   const { input } = await request.json();
+
+  //hacky solution to get image from url
   const imageUrl =
     "https://raw.githubusercontent.com/samuelahmed/CanvasCloak/main/public/chiMask.png";
-    const response = await fetch(imageUrl);
-    const blob = await response.blob();
+  const response = await fetch(imageUrl);
+  const blob = await response.blob();
+  const formData = new FormData();
+  formData.append("image", blob, "image.png");
 
-    console.log(`Image size: ${blob.size} bytes`);
-
-  
-    const formData = new FormData();
-    formData.append("image", blob, "image.png");
-  
-    const result = await openai.images.edit({
-      image: formData.get("image"),
-      prompt: input,
-    });
- 
-
-
-  console.log("ImageEdit Called");
-  console.log(result);
+  const result = await openai.images.edit({
+    image: formData.get("image"),
+    prompt: input,
+  });
 
   return new Response(JSON.stringify({ image: result }), { status: 200 });
 }
